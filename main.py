bb10038bb9e221cc04eb7451d7ac1c3da318087b
Shuxin Zheng (MSR Student-Person Consulting) <FAREAST.v-shuzhe@GCRGDL082.redmond.corp.microsoft.com>
2017-06-07 23:46:26 -0700

    add multiverso support

--- main.py
+++ main.py
@@ -14,14 +14,33 @@ import os
 import argparse
 
 from models import *
-from utils import progress_bar
+
 from torch.autograd import Variable
 
+import numpy as np
+import multiverso as mv
+from multiverso.torch_ext import torchmodel
+
+mv.init(sync=False, updater=b"sgd")
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
-parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+parser.add_argument('--arch', '-a', metavar='ARCH', default='resnet18', type=str,
+                    help='model architecture: (default: resnet18)')
+parser.add_argument('--lr', type=float, default=0.1, metavar='LR',
+                    help='learning rate (default: 0.1)')
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
+parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                    help='input batch size for training (default: 64)')
+parser.add_argument('--momentum', type=float, default=0, metavar='M',
+                    help='SGD momentum (default: 0)')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--worker', type=int, default=1, metavar='W',
+                    help='number of data loader worker (default: 1)')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                    help='how many batches to wait before logging training status')
 args = parser.parse_args()
+torch.manual_seed(args.seed)
 
 use_cuda = torch.cuda.is_available()
 best_acc = 0  # best test accuracy
@@ -41,11 +60,11 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=False, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+trainset = torchvision.datasets.CIFAR10(root='../data', train=True, download=True, transform=transform_train)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=args.worker)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=transform_test)
-testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+testset = torchvision.datasets.CIFAR10(root='../data', train=False, download=True, transform=transform_test)
+testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, shuffle=False, num_workers=args.worker)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
@@ -60,16 +79,25 @@ if args.resume:
     start_epoch = checkpoint['epoch']
 else:
     print('==> Building model..')
-    # net = VGG('VGG19')
-    # net = ResNet18()
-    # net = GoogLeNet()
-    # net = DenseNet121()
-    # net = ResNeXt29_2x64d()
-    net = MobileNet()
+    if args.arch=='mobilenet':
+        net = MobileNet()
+    elif args.arch==('resnet18'):
+        net = ResNet18()
+    elif args.arch==('resnet34'):
+        net = ResNet34()
+    elif args.arch==('resnet50'):
+        net = ResNet50()
+    elif args.arch==('resnet101'):
+        net = ResNet101()
+    elif args.arch==('resnet152'):
+        net = ResNet152()
+    else:
+        net = ResNet18()
+
+net = torchmodel.MVTorchModel(net)
 
 if use_cuda:
     net.cuda()
-    net = torch.nn.DataParallel(net, device_ids=range(torch.cuda.device_count()))
     cudnn.benchmark = True
 
 criterion = nn.CrossEntropyLoss()
@@ -83,22 +111,29 @@ def train(epoch):
     correct = 0
     total = 0
     for batch_idx, (inputs, targets) in enumerate(trainloader):
-        if use_cuda:
-            inputs, targets = inputs.cuda(), targets.cuda()
-        optimizer.zero_grad()
-        inputs, targets = Variable(inputs), Variable(targets)
-        outputs = net(inputs)
-        loss = criterion(outputs, targets)
-        loss.backward()
-        optimizer.step()
+        if batch_idx % mv.workers_num() == mv.worker_id():
+            if use_cuda:
+                inputs, targets = inputs.cuda(), targets.cuda()
+            optimizer.zero_grad()
+            inputs, targets = Variable(inputs), Variable(targets)
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+            loss.backward()
+            optimizer.step()
 
-        train_loss += loss.data[0]
-        _, predicted = torch.max(outputs.data, 1)
-        total += targets.size(0)
-        correct += predicted.eq(targets.data).cpu().sum()
+            net.cpu()
+            net.mv_sync()
+            net.cuda()
 
-        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-            % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+            train_loss += loss.data[0]
+            _, predicted = torch.max(outputs.data, 1)
+            total += targets.size(0)
+            correct += predicted.eq(targets.data).cpu().sum()
+
+            if (batch_idx/mv.workers_num()) % args.log_interval == 0:
+                print('Worker: {}\tTrain Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    mv.worker_id(), epoch, batch_idx * len(inputs), len(trainloader.dataset),
+                    100. * batch_idx / len(trainloader), loss.data[0]))
 
 def test(epoch):
     global best_acc
@@ -118,15 +153,18 @@ def test(epoch):
         total += targets.size(0)
         correct += predicted.eq(targets.data).cpu().sum()
 
-        progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-            % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+    print('\nWorker: {}\tTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        mv.worker_id(), test_loss, correct, len(testloader.dataset),
+        100. * correct / len(testloader.dataset)))
 
     # Save checkpoint.
     acc = 100.*correct/total
     if acc > best_acc:
         print('Saving..')
+        if use_cuda:
+            net.cpu()
         state = {
-            'net': net.module if use_cuda else net,
+            'net': net,
             'acc': acc,
             'epoch': epoch,
         }
@@ -134,8 +172,12 @@ def test(epoch):
             os.mkdir('checkpoint')
         torch.save(state, './checkpoint/ckpt.t7')
         best_acc = acc
+        if use_cuda:
+            net.cuda()
 
 
 for epoch in range(start_epoch, start_epoch+200):
     train(epoch)
     test(epoch)
+
+mv.shutdown()
